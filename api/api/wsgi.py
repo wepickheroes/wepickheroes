ce80ef2080d3b10229bb55b445a8d60d75b0080d
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-11-07 20:05:48 -0500

    Reformat django project

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for wph_server project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wph_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 application = get_wsgi_application()
