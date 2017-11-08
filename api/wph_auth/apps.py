dc033a6b254695b761edba09f1aa995b63b5846d
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-11-07 23:33:30 -0500

    Add auth app

---
+++ api/wph_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WphAuthConfig(AppConfig):
+    name = 'wph_auth'
---
+++ api/wph_auth/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+
+# class WPHUser(AbstractBaseUser, PermissionsMixin, UUIDModel):
+#     pass
