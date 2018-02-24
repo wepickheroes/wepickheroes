5d72402bfdb6dd09bcfff3e38886b2854a02ce41
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-02-24 13:51:40 -0500

    Flesh out team register page

--- client/src/components/user/SignupComplete.js
+++ client/src/components/user/SignupComplete.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Route } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import styled from 'styled-components'
 import { Button, Col, Container, Form, FormGroup, FormText, Label, Input, Row } from 'reactstrap'
 import { createUrl } from '../../api/utils'
@@ -7,25 +9,41 @@ const ContentContainer = styled.div`
     margin: 2rem 0;
 `
 
+const RegisterTeamQuestion = props => (
+    <Fragment>
+        <h2 className='text-center'>
+            Would you like to register a team?
+        </h2>
+        <div className='text-center'>
+            <LinkContainer to='/'>
+                <Button color="secondary" size="lg">No</Button>
+            </LinkContainer>
+            {' '}
+            <LinkContainer to='/signup-complete/team'>
+                <Button color="primary" size="lg">Yes</Button>
+            </LinkContainer>
+        </div>
+    </Fragment>
+)
+
+const RegisterTeamForm = props => (
+    <Fragment>
+        <h2 className='text-center'>
+            Register a team
+        </h2>
+    </Fragment>
+)
+
 const SignupComplete = props => (
     <ContentContainer>
         <Container>
             <h1 className='text-center'>Signup Complete!</h1>
-
-
             <Row>
                 <Col sm='12' md={{ size: '6', offset: '3' }} lg={{ size: '8', offset: '2' }}>
-                    <h2>
-                        Would you like to register a team?
-                    </h2>
-                    <Form>
-                        <FormGroup>
-                            <Label>Do you already have a team?</Label>
-                        </FormGroup>
-                    </Form>
+                    <Route path="/signup-complete" component={RegisterTeamQuestion} exact />
+                    <Route path="/signup-complete/team" component={RegisterTeamForm} exact />
                 </Col>
             </Row>
-
         </Container>
     </ContentContainer>
 )
