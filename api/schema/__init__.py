364d0001e993cfb8ad7e313a00c273647067bb8b
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-03-17 13:57:11 -0400

    Add CreateTeam mutation

--- api/schema/__init__.py
+++ api/schema/__init__.py
@@ -1,4 +1,5 @@
 import graphene
 from schema.queries import Query
+from schema.mutations import Mutations
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutations)
