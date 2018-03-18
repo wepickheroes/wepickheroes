c02750ee1fd664cf6a4207b86ed29ade26a4ce0d
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-03-18 10:31:04 -0400

    Add schedule models to admin

--- api/schedule/admin.py
+++ api/schedule/admin.py
@@ -7,4 +7,95 @@ from .models import (
 )
 
 
-# class LeagueSeasonAdmin(admin.ModelAdmin):
+class LeagueSeasonAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'number', 'start_date', 'end_date', )
+    readonly_fields = ('number', )
+
+
+class LeagueSeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'league_season',
+        'team_a',
+        'team_b',
+        'num_games',
+        'start_date',
+        'end_date',
+    )
+    list_filter = (
+        'league_season',
+        'num_games',
+        'start_date',
+        'end_date',
+    )
+    search_fields = (
+        'team_a__name',
+        'team_b__name',
+    )
+    raw_id_fields = (
+        'team_a',
+        'team_b',
+    )
+    readonly_fields = ('created', 'updated', )
+    fieldsets = (
+        ('Management', {
+            'fields': ('league_season', 'num_games', 'created', 'updated', )
+        }),
+        ('Dates', {
+            'fields': ('start_date', 'end_date', ),
+        }),
+        ('Teams', {
+            'fields': ('team_a', 'team_b', )
+        }),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related(
+            'team_a',
+            'team_b',
+            'league_season',
+        )
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'series',
+        'team_a_score',
+        'team_b_score',
+        'match_date',
+    )
+    list_filter = (
+        'match_date',
+    )
+    search_fields = (
+        'series__team_a__name',
+        'series__team_b__name',
+    )
+    readonly_fields = ('created', 'updated', )
+    raw_id_fields = ('series', )
+    fieldsets = (
+        ('Management', {
+            'fields': ('series', 'created', 'updated',)
+        }),
+        ('Dates', {
+            'fields': ('match_date',)
+        }),
+        ('Scores', {
+            'fields': ('team_a_score', 'team_b_score',),
+        }),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related(
+            'series__team_a',
+            'series__team_b',
+            'series__league_season',
+        )
+
+
+admin.site.register(LeagueSeason, LeagueSeasonAdmin)
+admin.site.register(LeagueSeries, LeagueSeriesAdmin)
+admin.site.register(Match, MatchAdmin)
--- api/schedule/models.py
+++ api/schedule/models.py
@@ -4,7 +4,7 @@ from nucleus.models import AbstractBaseModel
 
 
 class LeagueSeason(AbstractBaseModel):
-    number = models.PositiveIntegerField(unique=True, null=True)
+    number = models.PositiveIntegerField(unique=True, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
 
@@ -16,12 +16,18 @@ class LeagueSeason(AbstractBaseModel):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        dateformat = "%d/%m/%y"
-        return 'Season: {} vs. {}'.format(self.start_date.strftime(dateformat), self.end_date.strftime(dateformat))
+        dateformat = "%m/%d/%y"
+        datestring = self.start_date and self.end_date and (
+            " ({}-{})".format(self.start_date.strftime(dateformat) if self.start_date else '?',
+                              self.end_date.strftime(dateformat) if self.end_date else '?')
+        )
+        return 'Season {}{}'.format(
+            self.number, datestring or ''
+        )
 
 
 class LeagueSeries(AbstractBaseModel):
-    league_season = models.ForeignKey('schedule.LeagueSeason', null=True, blank=True, on_delete=models.SET_NULL)
+    league_season = models.ForeignKey('schedule.LeagueSeason', on_delete=models.CASCADE)
     num_games = models.PositiveIntegerField(blank=False, null=False, default=1)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
@@ -30,15 +36,21 @@ class LeagueSeries(AbstractBaseModel):
     team_b = models.ForeignKey('teams.Team', null=True, blank=True, related_name='series_played_as_team_2',
                                on_delete=models.SET_NULL)
 
+    class Meta:
+        verbose_name_plural = 'League series'
+
     def __str__(self):
         return 'Series: {} vs. {}'.format(self.team_a.name, self.team_b.name)
 
 
 class Match(AbstractBaseModel):
-    series = models.ForeignKey('schedule.LeagueSeries', null=True, blank=True, on_delete=models.SET_NULL)
+    series = models.ForeignKey('schedule.LeagueSeries', on_delete=models.CASCADE)
     team_a_score = models.PositiveIntegerField(blank=True, null=True)
     team_b_score = models.PositiveIntegerField(blank=True, null=True)
-    match_date = models.DateField(null=True,blank=True)
+    match_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Matches'
 
     def __str__(self):
         return 'Match: {} vs. {}'.format(self.series.team_a.name, self.series.team_b.name)
