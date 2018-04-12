c61145d2cb70c3d9f4bdd6d614f5b7aa7f4ba933
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-04-11 22:02:21 -0400

    Add sub count column to team admin

--- api/teams/admin.py
+++ api/teams/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from .models import Team
 from nucleus.admin import TeamMemberInline
+from nucleus.models import Role
 
 
 class TeamAdmin(admin.ModelAdmin):
@@ -11,6 +12,7 @@ class TeamAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'get_player_count',
+        'get_sub_count',
         'get_player_list',
         'created',
         'updated',
@@ -20,13 +22,19 @@ class TeamAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         return queryset.prefetch_related('players').annotate(
-            player_count=Count('players')
+            player_count=Count('teammember', filter=Q(teammember__role=Role.PLAYER)),
+            sub_count=Count('teammember', filter=Q(teammember__role=Role.SUB)),
         )
 
     def get_player_count(self, obj):
         return obj.player_count
     get_player_count.short_description = 'Num Players'
     get_player_count.admin_order_field = 'player_count'
+
+    def get_sub_count(self, obj):
+        return obj.sub_count
+    get_sub_count.short_description = 'Num Subs'
+    get_sub_count.admin_order_field = 'sub_count'
 
     def get_player_list(self, obj):
         return ', '.join([p.username for p in obj.players.all()])
