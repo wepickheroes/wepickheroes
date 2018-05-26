eb50dbc295f56f46535929d8b230f35b7f687c08
ruiqi mao <iamruiqi@gmail.com>
2018-05-26 10:42:47 -0400

    added qualifer bracket to homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
-import { Button, Card, CardBody, CardTitle, Col, Container, Row } from 'reactstrap'
+import { Button, Card, CardBody, CardTitle, Col, Container, Row, CardImg } from 'reactstrap'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
@@ -8,6 +8,7 @@ import { faSteam } from '@fortawesome/fontawesome-free-brands'
 
 import logoLarge from '../img/logos/WPHlogo5sgcolors.svg'
 import keyboardImg from '../img/gaming_stock.jpg'
+import openQualImg from '../img/Open_Qualifiersv1.png'
 import theDire from '../img/the_dire.jpg'
 import { createUrl } from '../api/utils'
 import {
@@ -37,14 +38,12 @@ const MainLogo = styled.img`
 const MarketingList = styled.ol`
     margin-left: 2rem;
 `
-
-
 const Homepage = props => {
     const { data: {loading, isAuthenticated = false } } = props
     return (
-        <Fragment style={{ margin: '1.875rem 0' }}>
+        <Fragment>
             <Container>
-                <Intro style={{ margin: '0' }}>
+                <Intro>
                     <MainLogo src={logoLarge} alt="We Pick Heroes" />
                     <h2>Competitive Amateur Dota 2 League</h2>
                     <Description className="lead">
@@ -55,39 +54,23 @@ const Homepage = props => {
                     <div>
                         {!loading && !isAuthenticated && (
                             <Button color="success" size="lg"
-                                href={createUrl('/login/steam/')}>
+                                    href={createUrl('/login/steam/')}>
                                 <FontAwesomeIcon icon={faSteam} />&nbsp;Sign Up
                             </Button>
                         )}
                     </div>
                 </Intro>
             </Container>
-            <Divider style={{ margin: '0' }}/>
+            <Divider/>
             <Container>
                 <Card>
                     <CardBody>
                         <CardTitle>
                             <span id="how-it-works" className="display-4">
-                                New Tournament League!
+                                Open Qualifier Bracket
                             </span>
                         </CardTitle>
-                        <Row>
-                            <Col md="4" xs="12">
-                                <MarketingImage className='img-fluid' src={keyboardImg}/>
-                            </Col>
-                            <Col md="8" xs="12" className="d-flex align-items-center">
-                                <div>
-                                    <MarketingDescription>
-                                        Sign-ups are now open for our new double-elimination bracket league!
-                                        We will be hosting a weekend tournament between <strong>June 2nd</strong> and <strong>June 17th</strong>,
-                                        with open qualifiers on <strong>May 26th</strong>.
-                                        We will have 8 teams competing for a <strong>$175</strong> prize pool.
-                                        For more information on the format, rules, and FAQ,
-                                        please check out the Resources tab - <strong><em>register now</em></strong>!
-                                    </MarketingDescription>
-                                </div>
-                            </Col>
-                        </Row>
+                                <CardImg className='img-fluid' src={openQualImg}/>
                     </CardBody>
                 </Card>
             </Container>
